a4f71fb5ac64fc90cce29b2ca4ad7ddd60864f7b
metakme <the_kme@hotmail.com>
2021-02-16 15:44:59 +0100

    refactoring and created 3 common versions of ERC20

--- contracts/ERC20MintBurnPauseRoles.sol
+++ contracts/ERC20MintBurnPauseRoles.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.6.7;
 
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
-contract Token is ERC20PresetMinterPauser {
+contract ERC20MintBurnPauseRoles is ERC20PresetMinterPauser {
     constructor(string memory name, string memory symbol) public ERC20PresetMinterPauser (name, symbol) {
     }
 }
